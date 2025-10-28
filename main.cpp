ab255e7b5e11bf93fefc40be31fb6bc36bca5c1d
André Schnabel <aschnabel@gams.com>
2025-10-28 15:37:32 +0100

    Build GDX as part of pipeline and rudimentary benchmark

--- main.cpp
+++ main.cpp
@@ -1,49 +1,24 @@
 #include <benchmark/benchmark.h>
-#include <cstring>
 #include <string>
-/*#include <gdx/gdx.hpp>
+#include <gdx.hpp>
 
 static void BM_gdxSimpleWrite(benchmark::State &state)
 {
     std::string msg;
     gdx::TGXFileObj gdx{msg};
-}*/
-
-// Define the function under test
-static void BM_StringCopy(benchmark::State &state)
-{
-    std::string x = "hello";
-
-    // Google benchmark relies on state.begin() and state.end() to run the benchmark and count iterations
-    for (auto _ : state)
+    std::array<int, 20> keys{};
+    std::array<double, 5> values{};
+    int errNr{};
+    gdx.gdxOpenWrite("bigfile.gdx", "benchmark", errNr);
+    gdx.gdxDataWriteRawStart("a", "", 0, 0, 0);
+    for (int i{}; i < 1000; i++)
     {
-        std::string copy(x);
-
-        // Use DoNotOptimize and ClobberMemory to prevent the compiler optimizing away your benchmark
-        // See: https://google.github.io/benchmark/user_guide.html#preventing-optimization
-        benchmark::DoNotOptimize(copy);
-        benchmark::ClobberMemory();
+        gdx.gdxDataWriteRaw(keys.data(), values.data());
     }
+    gdx.gdxDataWriteDone();
+    gdx.gdxClose();
 }
-// Register the benchmarked to be called by the executable
-BENCHMARK(BM_StringCopy);
-
-static void BM_memcpy(benchmark::State &state)
-{
-    char *src = new char[state.range(0)];
-    char *dst = new char[state.range(0)];
-    memset(src, 'x', state.range(0));
-    for (auto _ : state)
-    {
-        memcpy(dst, src, state.range(0));
-        benchmark::DoNotOptimize(dst);
-        benchmark::ClobberMemory();
-    }
-    delete[] src;
-    delete[] dst;
-}
-
-BENCHMARK(BM_memcpy)->Range(8, 8 << 10);
+BENCHMARK(BM_gdxSimpleWrite);
 
 // Entrypoint of the benchmark executable
 BENCHMARK_MAIN();
